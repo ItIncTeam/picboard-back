cc3862fe30e63b7402d5f22e80f77a83075aa535
GovorI <bell4el@mail.ru>
2026-05-11 17:38:33 +0300

    fix: settings webhook

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Test deploy is done!';
+    return 'Test deploy is done!!!';
   }
 }
