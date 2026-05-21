c3b454621691c080a898c6e4c82cc045eba0357f
GovorI <bell4el@mail.ru>
2026-05-21 20:58:44 +0300

    fix(app.controller.spec.ts): should return Test deploy is done!!!

--- apps/picboard-back/src/app.controller.spec.ts
+++ apps/picboard-back/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Test deploy is done!!!"', () => {
+      expect(appController.getHello()).toBe('Test deploy is done!!!');
     });
   });
 });
