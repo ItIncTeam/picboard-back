997e81e25995028bd528905459cbceb92f0e7cf1
GovorI <bell4el@mail.ru>
2026-07-08 21:45:09 +0300

    fix(gateway): csrfPrevention: false

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -34,7 +34,7 @@ import { PicboardDataSource } from './auth/picboard-data-source';
             methods: ['POST', 'OPTIONS'],
             allowedHeaders: ['Content-Type', 'Authorization'],
           },
-          csrfPrevention: true,
+          csrfPrevention: false,
           context: ({ req, res }: { req: any; res: any }) => ({ req, res }),
           /*context: ({ req, res }: { req: any; res: any }) => {
             console.log('gateway context auth', req.headers.authorization);
