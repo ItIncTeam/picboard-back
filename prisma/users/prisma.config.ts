489a55a22b2a4abcfc44614eacdb1fb82b06bca0
GovorI <bell4el@mail.ru>
2026-07-10 23:34:22 +0300

    fix(users-micro): prisma.config.ts

--- prisma/users/prisma.config.ts
+++ prisma/users/prisma.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: './schema.prisma',
@@ -7,7 +7,7 @@ export default defineConfig({
   },
   datasource: {
     url:
-      env('DATABASE_DIRECT_URL') ||
+      process.env.DATABASE_DIRECT_URL ||
       'postgresql://dummy:dummy@localhost:5432/dummy',
   },
 });
